945db29661851822aeff453b8bba8684c2b465cd
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2017-05-16 17:44:27 -0400

    fixes server connection issue

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,12 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL || {
     database: "burgers_db"
 });
 
-connection.connect();
+connection.connect(function(err){
+	if (err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id" + connection.threadId);
+});
 
 module.exports = connection;
